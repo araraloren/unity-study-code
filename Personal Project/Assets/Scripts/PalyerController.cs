4512716b9cd7f06baa2803cf8f6a36b0ebd3343e
loren <blackcatoverwall@gmail.com>
2023-10-27 19:28:20 +0800

    add moving for personal project, lab 3

--- Personal Project/Assets/Scripts/PalyerController.cs
+++ Personal Project/Assets/Scripts/PalyerController.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class PalyerController : MonoBehaviour
 {
+    public float lowSpeed = 1;
+    public float highSpeed = 8;
+    public float minPositionZ = -4;
+
+    private float speed;
+    private Vector3 movingDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = lowSpeed;
+        movingDirection = Vector3.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetKey(KeyCode.UpArrow);
+        // if player out of bound, change moving direction
+        if (transform.position.z < minPositionZ)
+        {
+            ChangeDirection();
+        }
+        else
+        {
+            // if right arrow pressed, change moving direction
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ChangeDirection();
+            }
+            // If left pressed, using high speed
+            if (Input.GetAxis("Left") < 0)
+            {
+                speed = highSpeed;
+            }
+            // otherwise using low speed
+            else
+            {
+                speed = lowSpeed;
+            }
+        }
+        transform.Translate(movingDirection * Time.deltaTime * speed);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        var obj = collision.gameObject;
+
+        if (obj.CompareTag("Potato"))
+        {
+            ChangeDirection();
+        }
+    }
+
+    /// <summary>
+    ///  Change the player moving direction
+    /// </summary>
+    public void ChangeDirection()
+    {
+        if (movingDirection == Vector3.forward)
+        {
+            movingDirection = Vector3.back;
+        }
+        else
+        {
+            movingDirection = Vector3.forward;
+        }
     }
 }
